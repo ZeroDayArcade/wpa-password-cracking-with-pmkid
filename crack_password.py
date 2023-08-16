5f1ea8469a47beb773177f6fe4d66f14482a804a
zerodayarcade.com <141867962+ZeroDayArcade@users.noreply.github.com>
2023-08-16 13:55:16 -0700

    Update crack_password.py

--- crack_password.py
+++ crack_password.py
@@ -1,6 +1,24 @@
 import hashlib, hmac, struct, sys
 import pbkdf2               
 
+# Default Values
+pmkid="4d4fe7aac3a2cecab195321ceb99a7d0"
+essid=b"hashcat-essid" 
+mac_ap=b"\xfc\x69\x0c\x15\x82\x64" 
+mac_cl=b"\xf4\x74\x7f\x87\xf9\xf4" 
+passlist_src="passlist.txt"
+
+# User Supplied Values
+if len(sys.argv) > 1: pmkid=sys.argv[1]
+if len(sys.argv) > 2: essid=bytes(sys.argv[2], 'utf-8')
+if len(sys.argv) > 3: mac_ap = bytes.fromhex("".join(sys.argv[3].replace("-", ":").split(":")))
+if len(sys.argv) > 4: mac_cl = bytes.fromhex("".join(sys.argv[4].replace("-", ":").split(":")))
+if len(sys.argv) > 5: passlist_src=sys.argv[5]
+
+# Read passlist.txt into a python list
+with open(passlist_src, 'r') as f:
+  passlist = f.read().splitlines()
+
 def crack_password(pmkid, essid, mac_ap, mac_cl, passlist):
     print('\033[95m')
     print("PMKID:                    ", pmkid)
@@ -27,21 +45,5 @@ def crack_password(pmkid, essid, mac_ap, mac_cl, passlist):
 
     print('\033[91m' + "\nFailed to crack password. " + 
           "It may help to try a different passwords list. " + '\x1b[0m' + "\n")
-
-pmkid="4d4fe7aac3a2cecab195321ceb99a7d0"
-essid=b"hashcat-essid" 
-mac_ap=b"\xfc\x69\x0c\x15\x82\x64" 
-mac_cl=b"\xf4\x74\x7f\x87\xf9\xf4" 
-passlist_src="passlist.txt"
-
-if len(sys.argv) > 1: pmkid=sys.argv[1]
-if len(sys.argv) > 2: essid=bytes(sys.argv[2], 'utf-8')
-if len(sys.argv) > 3: mac_ap = bytes.fromhex("".join(sys.argv[3].replace("-", ":").split(":")))
-if len(sys.argv) > 4: mac_cl = bytes.fromhex("".join(sys.argv[4].replace("-", ":").split(":")))
-if len(sys.argv) > 5: passlist_src=sys.argv[5]
-
-# Read passlist.txt into a python list
-with open(passlist_src, 'r') as f:
-  passlist = f.read().splitlines()
 
 crack_password(pmkid, essid, mac_ap, mac_cl, passlist)
